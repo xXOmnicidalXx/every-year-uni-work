c2561571b404c0cc24c5bf1094b4fa918ef8a6f4
luke <94399322+xXOmnicidalXx@users.noreply.github.com>
2021-11-25 22:54:25 +0000

    Add files via upload

---
+++ Year 2/Games Development/Games Assignment/Source/Games_Assignment/Games_Assignment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Games_Assignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Games_Assignment, "Games_Assignment" );
---
+++ Year 2/Games Development/Games Assignment/Source/Games_Assignment/Games_AssignmentGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Games_AssignmentGameModeBase.h"
+
