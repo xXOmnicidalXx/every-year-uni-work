8e813edbc5eb2d307aa65e0ed840ad98ee3c4769
luke <94399322+xXOmnicidalXx@users.noreply.github.com>
2021-11-25 22:49:02 +0000

    Delete Year 2/Games Development/Source directory

--- Year 2/Games Development/Source/Games_Assignment/Games_Assignment.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Games_Assignment.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Games_Assignment, "Games_Assignment" );
